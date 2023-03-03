c1dc94fac897402906e4d9136aab9a882e703456
barry <kooksee@163.com>
2023-03-03 23:01:10 +0800

    fix

--- lifecycle/builder.go
+++ lifecycle/builder.go
@@ -1,9 +1,18 @@
 package lifecycle
 
-func New(handlers []Handler) Lifecycle {
+type Exporter struct {
+	Lifecycle    Lifecycle
+	GetLifecycle GetLifecycle
+}
+
+func New(handlers []Handler) Exporter {
 	var lc = new(lifecycleImpl)
 	for i := range handlers {
 		handlers[i](lc)
 	}
-	return lc
+
+	return Exporter{
+		Lifecycle:    lc,
+		GetLifecycle: lc,
+	}
 }
