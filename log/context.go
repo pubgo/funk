5cae43b8e523ddd31ab9aed64781e8272adab0fc
kooksee <kooksee@163.com>
2023-04-14 08:57:02 +0800

    fix

--- log/context.go
+++ log/context.go
@@ -5,13 +5,21 @@ import "context"
 type ctxKey struct{}
 
 func WithCtx(ctx context.Context, log Logger) context.Context {
+	if log == nil {
+		panic("log is nil")
+	}
 	return context.WithValue(ctx, ctxKey{}, log)
 }
 
 // Ctx returns the Logger associated with the ctx.
-func Ctx(ctx context.Context) Logger {
+func Ctx(ctx context.Context, defLog ...Logger) Logger {
 	if l, ok := ctx.Value(ctxKey{}).(*loggerImpl); ok && l != nil {
 		return l
 	}
+
+	if len(defLog) > 0 && defLog[0] != nil {
+		return defLog[0]
+	}
+
 	return stdLog
 }
