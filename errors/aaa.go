691ddeeb143016ca622a19635e938aef304c72ed
kooksee <kooksee@163.com>
2023-04-20 08:22:56 +0800

    fix

--- errors/aaa.go
+++ errors/aaa.go
@@ -1,31 +1,11 @@
 package errors
 
 import (
-	"encoding/json"
-	"fmt"
-
 	"github.com/rs/zerolog"
 
 	"github.com/pubgo/funk/proto/errorpb"
 	"github.com/pubgo/funk/stack"
 )
-
-// base error
-// with message
-// with metadata
-// with tags
-// 一个底层error，用于展示，查询和和打印log
-// 一个wrap error，wrap一些主要的信息，用于参考和追踪错误链路
-// 一个code error
-// 错误是要能够被打印和输出的
-// 错误是要能够被识别和处理的
-func NewError(prefix string) {
-
-}
-
-type ErrCode1 struct {
-	code *errorpb.ErrCode
-}
 
 type Event = zerolog.Event
 type Tags map[string]any
@@ -46,22 +26,6 @@ type ErrIs interface {
 
 type ErrAs interface {
 	As(any) bool
-}
-
-type Error2 interface {
-	error
-	fmt.Stringer
-	json.Marshaler
-	WithField(key, value string) Error2
-	SetTag(key, value string) Error2
-	Event() *Event
-}
-
-type Error1 interface {
-	error
-	fmt.Stringer
-	ErrUnwrap
-	json.Marshaler
 }
 
 type Error interface {
