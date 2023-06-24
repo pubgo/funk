51e50165c77da3a9365ff63ec0133f360c676c83
百里(barry) <kooksee@163.com>
2023-06-24 18:19:49 +0800

    fix

---
+++ clients/bbolt/client.go
@@ -0,0 +1,93 @@
+package bbolt
+
+import (
+	"context"
+	"path/filepath"
+
+	bolt "go.etcd.io/bbolt"
+
+	"github.com/pubgo/funk/assert"
+	"github.com/pubgo/funk/config"
+	"github.com/pubgo/funk/errors"
+	"github.com/pubgo/funk/log"
+	"github.com/pubgo/funk/merge"
+	"github.com/pubgo/funk/pathutil"
+	"github.com/pubgo/funk/result"
+	"github.com/pubgo/funk/strutil"
+)
+
+func New(cfg *Config, log log.Logger) *Client {
+	cfg = merge.Copy(DefaultConfig(), cfg).Unwrap()
+
+	path := filepath.Join(config.GetConfigDir(), cfg.Path)
+	assert.Must(pathutil.IsNotExistMkDir(filepath.Dir(path)))
+	db := assert.Must1(bolt.Open(path, cfg.FileMode, cfg.getOpts()))
+
+	return &Client{DB: db, log: log}
+}
+
+type Client struct {
+	*bolt.DB
+	log log.Logger
+}
+
+func (t *Client) bucket(name string, tx *bolt.Tx) (*bolt.Bucket, error) {
+	_, err := tx.CreateBucketIfNotExists([]byte(name))
+	if err != nil {
+		return nil, errors.WrapKV(err, "bucket_name", name)
+	}
+
+	return tx.Bucket([]byte(name)), nil
+}
+
+func (t *Client) Set(ctx context.Context, key string, val []byte, names ...string) error {
+	return t.Update(ctx, func(bucket *bolt.Bucket) error {
+		return bucket.Put([]byte(key), val)
+	}, names...)
+}
+
+func (t *Client) Get(ctx context.Context, key string, names ...string) result.Result[[]byte] {
+	var (
+		val []byte
+		err = t.View(ctx, func(bucket *bolt.Bucket) error {
+			val = bucket.Get([]byte(key))
+			return nil
+		}, names...)
+	)
+
+	return result.Wrap(val, err)
+}
+
+func (t *Client) List(ctx context.Context, fn func(k, v []byte) error, names ...string) error {
+	return t.View(ctx, func(bucket *bolt.Bucket) error { return bucket.ForEach(fn) }, names...)
+}
+
+func (t *Client) Delete(ctx context.Context, key string, names ...string) error {
+	return t.Update(ctx, func(bucket *bolt.Bucket) error {
+		return bucket.Delete([]byte(key))
+	}, names...)
+}
+
+func (t *Client) View(ctx context.Context, fn func(*bolt.Bucket) error, names ...string) error {
+	name := strutil.GetDefault(names...)
+
+	return t.DB.View(func(tx *bolt.Tx) error {
+		bucket, err := t.bucket(name, tx)
+		if err != nil {
+			return err
+		}
+		return fn(bucket)
+	})
+}
+
+func (t *Client) Update(ctx context.Context, fn func(*bolt.Bucket) error, names ...string) error {
+	name := strutil.GetDefault(names...)
+
+	return t.DB.Update(func(tx *bolt.Tx) (err error) {
+		bucket, err := t.bucket(name, tx)
+		if err != nil {
+			return err
+		}
+		return fn(bucket)
+	})
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/pubgo/funk
 go 1.19
 
 require (
+	dario.cat/mergo v1.0.0
 	github.com/a8m/envsubst v1.3.0
 	github.com/alecthomas/repr v0.2.0
 	github.com/dave/jennifer v1.6.0
@@ -27,11 +28,13 @@ require (
 	github.com/stretchr/testify v1.8.1
 	github.com/valyala/fastrand v1.1.0
 	github.com/valyala/fasttemplate v1.2.2
+	go.etcd.io/bbolt v1.3.7
 	go.uber.org/atomic v1.10.0
 	golang.org/x/crypto v0.4.0
 	golang.org/x/exp v0.0.0-20221114191408-850992195362
 	google.golang.org/grpc v1.51.0
 	google.golang.org/protobuf v1.28.1
+	gopkg.in/yaml.v3 v3.0.1
 	modernc.org/sqlite v1.18.0
 )
 
@@ -53,7 +56,6 @@ require (
 	golang.org/x/tools v0.2.0 // indirect
 	google.golang.org/genproto v0.0.0-20221027153422-115e99e71e1c // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/uint128 v1.1.1 // indirect
 	modernc.org/cc/v3 v3.36.0 // indirect
 	modernc.org/ccgo/v3 v3.16.6 // indirect
