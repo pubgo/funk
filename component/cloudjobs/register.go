e074b293cf31854b9f67e08e94bd4ab155b0adb2
百里(barry) <kooksee@163.com>
2024-09-21 22:14:07 +0800

    fix: barry 2024-09-21 22:14:07

--- component/cloudjobs/register.go
+++ component/cloudjobs/register.go
@@ -9,31 +9,60 @@ import (
 	"github.com/pubgo/funk/stack"
 	"github.com/pubgo/funk/typex"
 	"github.com/pubgo/funk/vars"
+	"github.com/rs/xid"
 	"github.com/rs/zerolog"
 	"github.com/samber/lo"
 	"google.golang.org/protobuf/proto"
 )
 
-func init() {
-	vars.Register("cloudjobs", func() any {
-		subjectAndPbType := typex.DoBlock1(func() map[string]string {
-			subjectAndPbType := make(map[string]string)
-			for k, v := range subjects {
-				subjectAndPbType[k] = string(v.ProtoReflect().Descriptor().FullName())
-			}
-			return subjectAndPbType
-		})
+func (c *Client) RegisterVars() {
+	vars.Register("cloudjobs_"+xid.New().String(),
+		func() any {
+			subjectAndPbType := typex.DoBlock1(func() map[string]string {
+				subjectAndPbType := make(map[string]string)
+				for k, v := range subjects {
+					subjectAndPbType[k] = string(v.ProtoReflect().Descriptor().FullName())
+				}
+				return subjectAndPbType
+			})
 
-		return map[string]any{
-			"all_register_subjects": subjectAndPbType,
-			"default_prefix":        DefaultPrefix,
-			"default_timeout":       DefaultTimeout,
-			"default_max_retry":     DefaultMaxRetry,
-			"default_retry_backoff": DefaultRetryBackoff,
-			"default_job_name":      defaultJobName,
-			"cloudJobDelayKey":      cloudJobDelayKey,
-		}
-	})
+			return map[string]any{
+				"all_register_subjects":  subjectAndPbType,
+				"default_prefix":         DefaultPrefix,
+				"default_timeout":        DefaultTimeout,
+				"default_max_retry":      DefaultMaxRetry,
+				"default_retry_backoff":  DefaultRetryBackoff,
+				"default_job_name":       defaultJobName,
+				"cloud_job_delay_key":    cloudJobDelayKey,
+				"default_concurrent":     defaultConcurrent,
+				"default_max_concurrent": defaultMaxConcurrent,
+				"default_min_concurrent": defaultMinConcurrent,
+				"config":                 c.p.Cfg,
+				"prefix":                 c.prefix,
+				"streams":                lo.Keys(c.streams),
+				"handlers": lo.MapValues(c.handlers,
+					func(value map[string]JobHandler[proto.Message], key string) map[string]string {
+						return lo.MapValues(value, func(value JobHandler[proto.Message], key string) string {
+							return stack.CallerWithFunc(value).String()
+						})
+					}),
+				// jobs map[string]map[string]map[string]*jobHandler
+				"jobs": lo.MapValues(c.jobs,
+					func(value map[string]map[string]*jobHandler, key string) map[string]map[string]any {
+						return lo.MapValues(value, func(value map[string]*jobHandler, key string) map[string]any {
+							return lo.MapValues(value, func(value *jobHandler, key string) any {
+								return map[string]any{
+									"name":    value.name,
+									"config":  value.cfg,
+									"handler": stack.CallerWithFunc(value.handler).String(),
+								}
+							})
+						})
+					}),
+				"consumers": lo.MapValues(c.consumers,
+					func(value map[string]*Consumer, key string) []string { return lo.Keys(value) }),
+			}
+		})
 }
 
 var subjects = make(map[string]proto.Message)
