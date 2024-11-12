771dbd3a4c9a249f38545fea1ba65e986b268f85
百里(barry) <kooksee@163.com>
2024-11-12 22:28:37 +0800

    fix: barry 2024-11-12 22:28:37

--- errors/z_err_code_test.go
+++ errors/z_err_code_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestErrCode(t *testing.T) {
-	err1 := errors.Wrap(errors.NewCodeErr(testcodepb.ErrCodeDbConn), "hello")
-	rr := errors.Is(err1, errors.NewCodeErr(testcodepb.ErrCodeDbConn))
+	err1 := errors.Wrap(errors.NewCodeErr(testcodepb.TestErrCodeDbConn), "hello")
+	rr := errors.Is(err1, errors.NewCodeErr(testcodepb.TestErrCodeDbConn))
 	if !rr {
 		t.Fatal("not match")
 	}
 
-	t.Log(errors.As(err1, testcodepb.ErrCodeDbConn))
+	t.Log(errors.As(err1, testcodepb.TestErrCodeDbConn))
 }
