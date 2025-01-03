b9ce90d250cf5ac8aa8cd2704a40e5e2a0f9e0cc
barry <kooksee@163.com>
2025-01-03 11:33:51 +0800

    fix: barry 2025-01-03 11:33:51

--- netutil/util.go
+++ netutil/util.go
@@ -3,12 +3,10 @@ package netutil
 import (
 	"errors"
 	"fmt"
-	"io"
 	"net"
 	"net/http"
 	"regexp"
 	"strings"
-	"syscall"
 	"time"
 
 	"github.com/pubgo/funk/assert"
@@ -61,44 +59,6 @@ func DiscoverDNS(service, proto, address string) ([]*net.SRV, error) {
 	}
 
 	return addresses, nil
-}
-
-var errUnexpectedRead = errors.New("unexpected read from socket")
-
-func ConnCheck(conn net.Conn) error {
-	var sysErr error
-
-	sysConn, ok := conn.(syscall.Conn)
-	if !ok {
-		return nil
-	}
-
-	rawConn, err := sysConn.SyscallConn()
-	if err != nil {
-		return err
-	}
-
-	err = rawConn.Read(func(fd uintptr) bool {
-		var buf [1]byte
-		n, err := syscall.Read(int(fd), buf[:])
-		switch {
-		case n == 0 && err == nil:
-			sysErr = io.EOF
-		case n > 0:
-			sysErr = errUnexpectedRead
-		case err == syscall.EAGAIN || err == syscall.EWOULDBLOCK:
-			sysErr = nil
-		default:
-			sysErr = err
-		}
-
-		return true
-	})
-	if err != nil {
-		return err
-	}
-
-	return sysErr
 }
 
 const (
