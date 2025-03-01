5bf95966828bfcb56a90bf3d765aac624cb713ea
barry <kooksee@163.com>
2025-03-01 19:38:02 +0800

    fix: barry quick fix, 2025-03-01 19:38:02

--- anyhow/aherrcheck/errcheck_test.go
+++ anyhow/aherrcheck/errcheck_test.go
@@ -1,12 +1,13 @@
 package aherrcheck
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func errCheck1(err error) error {
+func errCheck1(ctx context.Context, err error) error {
 	return err
 }
 
@@ -14,7 +15,9 @@ func TestErrCheck(t *testing.T) {
 	assert.Equal(t, len(GetErrCheckFrames()), 0)
 
 	RegisterErrCheck(errCheck1)
+	assert.Equal(t, len(GetErrCheckFrames()), 1)
 
+	RegisterErrCheck(errCheck1)
 	assert.Equal(t, len(GetErrCheckFrames()), 1)
 
 	RemoveErrCheck(errCheck1)
