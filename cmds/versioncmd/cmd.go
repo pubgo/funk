059ce8c6fd9d8cb7c4fe65426c4a13324d53d4c8
barry <kooksee@163.com>
2025-07-25 22:07:26 +0800

    fix: barry quick fix, 2025-07-25 22:07:26

--- cmds/versioncmd/cmd.go
+++ cmds/versioncmd/cmd.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	"github.com/urfave/cli/v3"
-	
+
 	"github.com/pubgo/funk/pretty"
 	"github.com/pubgo/funk/recovery"
 	"github.com/pubgo/funk/running"
@@ -18,12 +18,6 @@ func New() *cli.Command {
 		Usage: fmt.Sprintf("%s version info", version.Project()),
 		Action: func(ctx context.Context, command *cli.Command) error {
 			defer recovery.Exit()
-			fmt.Println("project:", version.Project())
-			fmt.Println("version:", version.Version())
-			fmt.Println("commit-id:", version.CommitID())
-			fmt.Println("build-time:", version.BuildTime())
-			fmt.Println("instance-id:", running.InstanceID)
-			fmt.Println("device-id:", running.DeviceID)
 			pretty.Println(running.GetSysInfo())
 			return nil
 		},
