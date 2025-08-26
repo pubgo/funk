d731cdff3864804ea11523fc0978bcee92fca44e
barry <kooksee@163.com>
2025-08-26 20:44:57 +0800

    fix: barry quick fix, 2025-08-26 20:44:57

--- v2/result/util.go
+++ v2/result/util.go
@@ -3,6 +3,7 @@ package result
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"runtime/debug"
 
 	"github.com/pubgo/funk/errors"
@@ -214,5 +215,7 @@ func setError(setter ErrSetter, err error) {
 		*errSet.err = err
 	case *Result[any]:
 		errSet.err = err
+	default:
+		slog.Error("Unwrap: error setter type error", "type", fmt.Sprintf("%T", setter))
 	}
 }
