0b8e18565a970b5e21c71dda6a2654effba63888
barry <kooksee@163.com>
2025-09-06 01:54:15 +0800

    fix: barry quick fix, 2025-09-06 01:54:15

--- protoutils/util.go
+++ protoutils/util.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 	"unicode"
 
-	pongo2 "github.com/flosch/pongo2/v6"
 	"github.com/pubgo/funk/assert"
 	"github.com/pubgo/funk/errors"
 	options "google.golang.org/genproto/googleapis/api/annotations"
@@ -279,16 +278,6 @@ func camel2Case(name string) string {
 
 func trim(s string) string {
 	return strings.Trim(strings.TrimSpace(s), ".-_/")
-}
-
-type Context = pongo2.Context
-
-func Template(tpl string, m pongo2.Context) string {
-	temp := assert.Must1(pongo2.FromString(tpl))
-
-	w := bytes.NewBuffer(nil)
-	assert.Must(temp.ExecuteWriter(m, w), tpl)
-	return w.String()
 }
 
 func goZeroValue(f *descriptorpb.FieldDescriptorProto) string {
