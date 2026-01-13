32aa8f013e91e9289fa3f741ea7ff42612c74865
barry <kookshine@gmail.com>
2026-01-13 18:31:24 +0800

    chore: quick update feat/feature_ext at 2026-01-13 18:31:24

--- debugs/debug.go
+++ debugs/debug.go
@@ -3,3 +3,11 @@ package debugs
 import "github.com/pubgo/funk/v2/features"
 
 var Enabled = features.Bool("debug.enabled", false, "feature: enable debug mode")
+
+func SetEnabled() {
+	_ = Enabled.Set("true")
+}
+
+func SetDisabled() {
+	_ = Enabled.Set("false")
+}
